1561a6709e399bb2f65db7afecea6e2675cbc846
Jeffrey Li <jeffreyli.us@gmail.com>
2018-02-04 11:05:43 -0500

    Renamed files.

--- Google Custom Search Engine/script.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-    url: 'http://news.bbc.co.uk',
-    type: 'GET',
-    success: function(res) {
-        var headline = $(res.responseText).find('a.tsh').text();
-        alert(headline);
-    }
-});
